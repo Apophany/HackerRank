fd4b5ae7583e8f74d57202261d475e8a6fbef582
Apophany <bagnallcf1@hotmail.com>
2018-04-15 18:19:01 +0100

    Solution for fraction to binary

---
+++ src/main/java/com/hacker_rank/algorithms/codinginterview/book/Ch5_BitManipulation.java
@@ -0,0 +1,45 @@
+package com.hacker_rank.algorithms.codinginterview.book;
+
+public class Ch5_BitManipulation {
+    /**
+     * 5.1 Insertion: Given two 32 bit numbers, N and M, and two bit positions,
+     * i and j. Insert M into N such that M starts at bit j and ends at bit i.
+     * You can assume the bits j through i have enough space to fit all of M.
+     * <p>
+     * E.g N = 10000000000, M = 10011, i = 2, j = 6
+     * Output: N = 10001001100
+     */
+    public static int insertMtoN(int m, int n, int i, int j) {
+        int mask = ~(((1 << j) - 1) << i);
+        return n & mask | m << i;
+    }
+
+    /**
+     * 5.2 Binary to string: Given a real number between 0 and 1 that is passed in
+     * as a double, print the binary representation. If the number cannot be
+     * represented accurately in binary with at most 32 characters, print 'ERROR'
+     */
+    public static String binaryToString(double n) {
+        if (n > 1 || n < 0) {
+            return "ERROR";
+        }
+        final StringBuilder builder = new StringBuilder(".");
+
+        double fraction = 0.5;
+        while (n > 0) {
+            if (builder.length() > 32) {
+                return "ERROR";
+            }
+
+            if (n >= fraction) {
+                builder.append(1);
+                n -= fraction;
+            } else {
+                builder.append(0);
+            }
+            fraction /= 2;
+        }
+
+        return builder.toString();
+    }
+}
---
+++ src/test/java/com/hacker_rank/algorithms/codinginterview/Ch5_BitManipulationTest.java
@@ -0,0 +1,29 @@
+package com.hacker_rank.algorithms.codinginterview;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.hacker_rank.algorithms.codinginterview.book.Ch5_BitManipulation.binaryToString;
+import static com.hacker_rank.algorithms.codinginterview.book.Ch5_BitManipulation.insertMtoN;
+
+public class Ch5_BitManipulationTest {
+
+    @Test
+    public void test_insertMToN() {
+        int m = Integer.parseInt("10011", 2);
+        int n = Integer.parseInt("10001111100", 2);
+        int i = 2;
+        int j = 6;
+
+        Assert.assertEquals("10001001100", Integer.toBinaryString(insertMtoN(m, n, i, j)));
+    }
+
+    @Test
+    public void test_binaryToString() {
+        Assert.assertEquals("ERROR", binaryToString(0.1));
+        Assert.assertEquals(".101", binaryToString(0.625));
+        Assert.assertEquals(".11", binaryToString(0.75));
+        Assert.assertEquals("ERROR", binaryToString(-0.5));
+        Assert.assertEquals("ERROR", binaryToString(1.5));
+    }
+}
