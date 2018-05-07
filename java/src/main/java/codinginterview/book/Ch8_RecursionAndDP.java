3f2be6687214f08bc88ca2051ee62eeb7e17b2a3
Apophany <bagnallcf1@hotmail.com>
2018-05-07 23:00:27 +0100

    Added solution for robot in a grid

--- java/src/main/java/codinginterview/book/Ch8_RecursionAndDP.java
+++ java/src/main/java/codinginterview/book/Ch8_RecursionAndDP.java
@@ -1,5 +1,10 @@
 package codinginterview.book;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Ch8_RecursionAndDP {
     /**
      * 8.1 Triple Step: A child is running up a staircase with n
@@ -18,11 +23,62 @@ public class Ch8_RecursionAndDP {
             return 0;
         }
         if (numWays[numSteps] == 0) {
-            numWays[numSteps] =
-                    tripleStep(numSteps - 1, numWays)
-                            + tripleStep(numSteps - 2, numWays)
-                            + tripleStep(numSteps - 3, numWays);
+            numWays[numSteps] = tripleStep(numSteps - 1, numWays)
+                    + tripleStep(numSteps - 2, numWays)
+                    + tripleStep(numSteps - 3, numWays);
         }
         return numWays[numSteps];
+    }
+
+    /**
+     * 8.2 Robot in a grid: Imagine a robot sitting on the upper left
+     * corner of the grid with r rows and c columns. The robot can only
+     * move in two directions, right and down, but certain cells are
+     * 'off limits' such that the robot cannot step on them. Design
+     * an algorithm to find a path for the robot from the top left to
+     * the bottom right.
+     */
+    public List<Point> robotInAGrid(boolean[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) {
+            throw new IllegalArgumentException("Grid must be at least 1x1");
+        }
+        final Map<Point, List<Point>> memo = new HashMap<>();
+        return robotInAGrid(grid, memo, grid.length - 1, grid[0].length - 1);
+    }
+
+    private List<Point> robotInAGrid(boolean[][] grid, Map<Point, List<Point>> memo, int x, int y) {
+        if (x < 0 || y < 0 || x > grid.length || y > grid[0].length) {
+            return null;
+        }
+        //Cell is off limit
+        if (!grid[x][y]) {
+            return null;
+        }
+        final Point point = new Point(x, y);
+        final boolean isOrigin = (x == 0 && y == 0);
+
+        if (memo.get(point) == null) {
+            List<Point> path = robotInAGrid(grid, memo, x - 1, y);
+            if (path == null || path.isEmpty()) {
+                path = robotInAGrid(grid, memo, x, y - 1);
+            }
+            if (path != null && (isOrigin || !path.isEmpty())) {
+                path.add(point);
+            } else {
+                path = new ArrayList<>();
+            }
+            memo.put(point, path);
+        }
+        return memo.get(point);
+    }
+
+    private class Point {
+        int x;
+        int y;
+
+        private Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
